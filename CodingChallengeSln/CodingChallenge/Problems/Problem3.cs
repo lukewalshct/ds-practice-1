a30a3c764002d690ee781724b6a65dc59b2360d5
Luke Walsh <lukewalshct@gmail.com>
2016-08-10 22:05:00 -0400

    started problem3

---
+++ CodingChallengeSln/CodingChallenge/Problems/Problem3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Problems
+{
+    public static class Problem3
+    {
+        public static int maxStep(int N, int K)
+        {
+            return addNextAction(N, K, 0, 1);    
+        }
+
+        private static int addNextAction(int totMoves, int badStep, int curStep, int moveCount)
+        {
+            if (moveCount == totMoves) return curStep;
+
+            if(curStep + moveCount == badStep)
+            {
+                return curStep + addNextAction(totMoves, badStep, curStep, moveCount + 1);
+            }
+            else
+            {
+                return curStep + moveCount + addNextAction(totMoves, badStep, curStep, moveCount + 1);
+            }
+        }
+    }
+}
