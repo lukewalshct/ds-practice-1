4bd4b3b465d4c41ffa548e81db46464acfb82cb0
Luke Walsh <lukewalshct@gmail.com>
2016-08-10 21:16:07 -0400

    added problem2

---
+++ CodingChallengeSln/CodingChallenge/Problems/Problem2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Problems
+{
+    public static class Problem2
+    {
+        public static int isPangram(string N)
+        {
+            if(N == null)
+            {
+                throw new ArgumentNullException("String cannot be null.");
+            }
+            if (N.Length == 0 || N.Length > 103)
+            {
+                throw new ArgumentOutOfRangeException("String must be between 1 and 103 characters.");
+            }
+
+            HashSet<char> remainingChars = createRemainingCharsSet();
+
+            for(int i = 0; i < N.Length; i++)
+            {
+                remainingChars.Remove(N[i]);
+            }
+
+            return remainingChars.Count == 0 ? 1 : 0;
+        }
+
+        private static HashSet<char> createRemainingCharsSet()
+        {
+            HashSet<char> remainingChars = new HashSet<char>();
+
+            for(char c = 'a'; c <= 'z'; c++)
+            {
+                remainingChars.Add(c);
+            }
+
+            return remainingChars;
+        }
+    }
+}
