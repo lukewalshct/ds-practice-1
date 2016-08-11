11ca4b989907fce0d3e200b515cfd5fecb2915dd
Luke Walsh <lukewalshct@gmail.com>
2016-08-10 21:24:22 -0400

    added preliminary test for problem2

--- CodingChallengeSln/CodingChallenge/Program.cs
+++ CodingChallengeSln/CodingChallenge/Program.cs
@@ -11,12 +11,24 @@ namespace CodingChallenge
     {
         static void Main(string[] args)
         {
+            //Problem1
             int[] a = new int[] { 3, 5, 7 };
             int[] b = new int[] { 2, 4, 6, 0, 0, 0 };
             printArray(a);
             printArray(b);
             Problem1.mergeArray(a, b, a.Length);
             printArray(b);
+
+            //Problem2
+            string n1 = "The quick brown fox jumps over the lazy dog";
+            string n2 = "The quick brown fox jumps the lazy dog";
+            string n3 = "We promptly judged antique ivory buckles for the next prize";
+            string n4 = "We promptly judged antique ivory buckles for the prize";
+
+            Console.WriteLine(Problem2.isPangram(n1));
+            Console.WriteLine(Problem2.isPangram(n2));
+            Console.WriteLine(Problem2.isPangram(n3));
+            Console.WriteLine(Problem2.isPangram(n4));
         }
 
         private static void printArray(int[] a)
