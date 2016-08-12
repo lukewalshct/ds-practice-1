f659bbac927ebda78fe84e4646ca80a7483955ed
Luke Walsh <ldwalsh@mathematica-mpr.com>
2016-08-12 10:36:34 -0400

    added prob3 unit tests

--- CodingChallengeSln/CodingChallenge/Problems/Problem3.cs
+++ CodingChallengeSln/CodingChallenge/Problems/Problem3.cs
@@ -8,9 +8,18 @@ namespace CodingChallenge.Problems
 {
     public static class Problem3
     {
-        public static int maxStep(int N, int K)
+        public static int maxStep(int totMoves, int badStep)
         {
-            return addNextAction(N, K, 0, 1);    
+            if(!(totMoves >=1 && totMoves <= 2000))
+            {
+                throw new ArgumentOutOfRangeException("Total moves must be between 1 and 2,000.");
+            }
+            if(!(badStep >= 1 && badStep <= 4000000))
+            {
+                throw new ArgumentOutOfRangeException("Bad step number must be between 1 and 4,000,000");
+            }           
+
+            return addNextAction(totMoves, badStep, 0, 1);    
         }
 
         private static int addNextAction(int totMoves, int badStep, int curStep, int moveCount)
---
+++ CodingChallengeTestSln/CodingChallengeTest/ProblemTests/Problem3Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingChallenge.Problems;
+
+namespace CodingChallengeTest.ProblemTests
+{
+    [TestClass]
+    public class Problem3Test
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+            "Total moves must be between 1 and 2,000.")]
+        public void IfMovesTooLowThrowArgException()
+        {
+            Problem3.maxStep(0, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+            "Total moves must be between 1 and 2,000.")]
+        public void IfMovesTooHighThrowArgException()
+        {
+            Problem3.maxStep(2001, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+            "Bad step number must be between 1 and 4,000,000")]
+        public void IfBadStepTooLowThrowArgException()
+        {
+            Problem3.maxStep(5, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),
+            "Bad step number must be between 1 and 4,000,000")]
+        public void IfBadStepTooHighThrowArgException()
+        {
+            Problem3.maxStep(5, 40000001);
+        }
+
+        [TestMethod]
+        public void TestBadStepSkippedOutput()
+        {            
+            Assert.AreEqual(3, Problem3.maxStep(2, 2));
+        }
+
+        [TestMethod]
+        public void TestBadStepHitOutput()
+        {
+            Assert.AreEqual(2, Problem3.maxStep(2, 1));
+        }
+
+        [TestMethod]
+        public void TestMinInputValues()
+        {
+            Assert.AreEqual(0, Problem3.maxStep(1, 1));
+        }
+
+        [TestMethod]
+        public void TestMaxInputValues()
+        {            
+            Assert.AreEqual(2001000, Problem3.maxStep(2000, 4000000));
+        }
+    }
+}
